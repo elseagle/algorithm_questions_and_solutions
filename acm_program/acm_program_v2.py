599eac744070e9069b57c4a7e8aa0760233942f3
Sogo Ogundowole <dowolebolu@gmail.com>
2022-04-26 03:01:29 +0100

    chore: optimize logic, remove loops

--- acm_program/acm_program_v2.py
+++ acm_program/acm_program_v2.py
@@ -2,6 +2,7 @@ from functools import reduce
 import itertools
 from re import sub
 import re
+
 #
 # Complete the 'acmTeam' function below.
 #
@@ -9,43 +10,55 @@ import re
 # The function accepts STRING_ARRAY topic as parameter.
 #
 
-people_dict = {1: [], 2: [], 3: [], 4: [], 5: []}
+people_dict = {}
 people_counter = 0
+all_checkers = []
+
+# logic to get what topics folks are knowledgeable about
 def extract_positions(topic_):
-        global people_counter, people_dict
-        people_counter +=1
-        knowledgeable_about = [m.start() for m in re.finditer('1', topic_)]
-        people_dict[people_counter] = []
-        people_dict[people_counter].extend(knowledgeable_about)
+    global people_counter, people_dict
+    people_counter += 1
+    knowledgeable_about = [m.start() for m in re.finditer("1", topic_)]
+    people_dict[people_counter] = []
+    people_dict[people_counter].extend(knowledgeable_about)
+
+
+def get_intersection(combination):
+    global all_checkers, people_dict
+    i, j = combination
+
+    # find the union of knowledge between a pair of participants
+    intersection = set(people_dict[int(i)]).union(set(people_dict[int(j)]))
+    all_checkers.append(len(intersection))
+
 
 def acmTeam(topic):
-    # Write your code here
-    global people_dict
-    
+    global people_dict, all_checkers
+
     _ = list(map(extract_positions, topic))
 
     all_checkers = []
-    pair_holder = []
-    all_combinations = list(itertools.product(range(1, len(topic) + 1), range(1, len(topic) + 1)))
-    for i,j in all_combinations:
+    all_combinations = list(
+        itertools.product(range(1, len(topic) + 1), range(1, len(topic) + 1))
+    )
+    # necessary conditions to filter out unneeded values
+    all_combinations = list(
+        filter(
+            lambda x: x[0] <= len(topic[1]) and x[1] <= len(topic) and x[0] != x[1],
+            all_combinations,
+        )
+    )
 
-            if (
-                i <= len(topic[1])
-                and j <= len(topic)
-                and i != j
-                and [i, j] not in pair_holder
-                and [j, i] not in pair_holder
-            ):
-                person_i_set = set(people_dict[i])
-                person_j_set = set(people_dict[j])
-                intersection = person_i_set.union(person_j_set)
-                pair_holder.append([i, j])
-                all_checkers.append(len(intersection))
-
-    print(all_checkers)
+    # drop duplicate tuple - using frozen set b/c tuples are not hashable
+    all_combinations = list(
+        set(list(set(frozenset(set(item)) for item in all_combinations)))
+    )
+    _ = list(map(get_intersection, all_combinations))
 
     return [len(topic[1]), all_checkers.count(len(topic[1]))]
 
 
-print(acmTeam(["10101", "11110", "00010", "00101"]))
-# print(acmTeam(["11101", "10101", "11001", "10111", "10000", "01110"]))
+# N.B: Run one testcase at a time to get correct output
+
+# print(acmTeam(["10101", "11110", "00010", "00101"]))
+print(acmTeam(["11101", "10101", "11001", "10111", "10000", "01110"]))
